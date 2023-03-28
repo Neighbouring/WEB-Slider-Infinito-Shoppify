1860d415058a8bd19428b2b0aaeafe3c341ea55a
Neighbouring <neighbouringco@gmail.com>
2023-03-27 22:49:09 -0300

    PerformanceObserver

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ $('article').hover(function () {
 $('article').blur(function () {
   $('article').toggleClass('rodar');
 });
+
+// https://web.dev/fcp/
+new PerformanceObserver((entryList) => {
+  for (const entry of entryList.getEntriesByName('first-contentful-paint')) {
+    console.log('FCP candidate:', entry.startTime, entry);
+  }
+}).observe({ type: 'paint', buffered: true });
